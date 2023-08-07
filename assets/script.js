1819e368477892dcee7e464a62932946c5a5654d
vincenzodivita <divitavincenzo@icloud.com>
2023-08-07 16:55:30 +0200

    Add operation var

--- assets/script.js
+++ assets/script.js
@@ -4,24 +4,32 @@ const acBtn = document.querySelector(".acBtn");
 const resultBtn = document.querySelector(".resultBtn");
 const operator = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
+let operation = "";
 
 
 numBtn.forEach(function(btn) {
     btn.addEventListener("click", function() {
-        display.innerHTML += btn.textContent
+        display.innerHTML += btn.textContent;
+        operation = display.textContent;
+        console.log(operation.split(' ').join('').split('x').join('*'));
     });
 });
 
 acBtn.addEventListener("click", function() {
-    display.innerHTML = ""
+    display.innerHTML = "";
+    operation = display.textContent;
+    console.log(operation.split(' ').join(''));
 });
 
 delBtn.addEventListener("click", function() {
     display.innerHTML = display.textContent.slice(0, -1);
+    operation = display.textContent;
+    console.log(operation.split(' ').join(''));
 });
 
 operator.forEach(function(btn) {
     btn.addEventListener("click", function() {
-        display.innerHTML += " " + btn.textContent + " "
+        display.innerHTML += " " + btn.textContent + " "  
     });
 });
+

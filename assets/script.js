debf3cff7a546d7672e84f3e8a91f127b3d39de0
vincenzodivita <divitavincenzo@icloud.com>
2023-08-07 16:30:44 +0200

    Add buttons querySelector

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const numBtn = document.querySelectorAll(".numBtn");
+const delBtn = document.querySelector(".delBtn");
+const acBtn = document.querySelector(".acBtn");
+const resultBtn = document.querySelector(".resultBtn");
+const operator = document.querySelectorAll(".operator");
+const display = document.querySelector(".display");
